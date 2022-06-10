2a88328a4c2884783d75660b5a9611d21d420eae
santygcp <97255596+santygcp@users.noreply.github.com>
2022-06-11 04:42:31 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -10,7 +10,7 @@ helm repo add santy https://voltdb-kubernetes-charts.storage.googleapis.com
 
 #creating a cluster
 
-#gcloud container clusters create  --machine-type "c2-standard-8" --image-type UBUNTU_CONTAINERD  --num-nodes 3    xdcr-1 --disk-type "pd-ssd" --disk-size "300" --zone "europe-west1-b" 
+gcloud container clusters create  --machine-type "c2-standard-8" --image-type UBUNTU_CONTAINERD  --num-nodes 3    xdcr-1 --disk-type "pd-ssd" --disk-size "300" --zone "europe-west1-b" 
 
 
 #connecting to the cluster
@@ -19,11 +19,12 @@ kubectl config use-context gke_fourth-epigram-293718_europe-west1-b_xdcr-1
 
 
 #create the secret registry
-#kubectl create secret docker-registry dockerio-registry --docker-username=jadejakajal13  --docker-password=b461d1b4-82c4-499e-afc0-f17943a16411  --docker-email=jadejakajal13@gmail.com
+
+kubectl create secret docker-registry dockerio-registry --docker-username=jadejakajal13  --docker-password=b461d1b4-82c4-499e-afc0-f17943a16411  --docker-email=jadejakajal13@gmail.com
 
 helm install xdcr1 --version=1.3.5 --values $VALUES1 --set operator.image.tag=1.3.5 --set-file cluster.config.licenseXMLFile=$LICENSE_FILE santy/voltdb
 
-sleep 360
+sleep 240
 
 kubectl create -f votertest.yaml
 
@@ -43,15 +44,15 @@ kubectl exec -it votertestfinal -- /bin/bash -c "cd /opt/voltdb/voter/ ; ./run.s
 #    echo "all job's completed"
 #fi
 
-#gcloud container clusters create  --machine-type "c2-standard-8" --image-type UBUNTU_CONTAINERD  --num-nodes 3    xdcr-2 --disk-type "pd-ssd" --disk-size "300" --zone "europe-west1-b"
+gcloud container clusters create  --machine-type "c2-standard-8" --image-type UBUNTU_CONTAINERD  --num-nodes 3    xdcr-2 --disk-type "pd-ssd" --disk-size "300" --zone "europe-west1-b"
 
 kubectl config use-context gke_fourth-epigram-293718_europe-west1-b_xdcr-2
 
-#kubectl create secret docker-registry dockerio-registry --docker-username=jadejakajal13  --docker-password=b461d1b4-82c4-499e-afc0-f17943a16411  --docker-email=jadejakajal13@gmail.com
+kubectl create secret docker-registry dockerio-registry --docker-username=jadejakajal13  --docker-password=b461d1b4-82c4-499e-afc0-f17943a16411  --docker-email=jadejakajal13@gmail.com
 
 helm install xdcr2 --version=1.3.5 --values $VALUES2 --set operator.image.tag=1.3.5 --set-file cluster.config.licenseXMLFile=$LICENSE_FILE santy/voltdb
 
-sleep 360
+sleep 240
 
 kubectl create -f votertest2.yaml
 
